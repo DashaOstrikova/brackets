4cea2e687a0b7764fe596feca1697d25f364e3ea
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-03-01 01:10:26 +0300

    feat: implement check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  let prev;
+  let brackets = bracketsConfig.map(bracket => bracket.join(''));
+  while(newStr){
+    prev = newStr.length;
+    brackets.forEach(bracket => {
+      newStr = newStr.replace(bracket,'');
+    });
+    if(prev === newStr.length){
+      break;
+    }
+  }
+  if(newStr === ''){
+    return true;
+  }
+  return false;
 }
